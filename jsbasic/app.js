60d227b2307aea7b3928239b81e7cdd864189c17
minthuhlaing <minthuhlaing786@gmail.com>
2024-09-11 15:08:14 +0630

    AND gate and OR gate

---
+++ jsbasic/app.js
@@ -0,0 +1,114 @@
+// variables
+
+// var name = "john doe";
+// name = "susan" // override lote tar
+// console.log(name);
+
+
+//Scope variable
+// let name = "john doe"
+// {
+//   let age = 22;
+//   console.log(name);
+//   console.log(age);
+// }
+// console.log(age);
+
+
+// const PI = 3.142;
+// PI = 22;
+// console.log(PI);
+
+
+// Datatype 
+
+// String
+// let name = "john doe"
+// console.log(typeof name);
+
+// number 
+
+// let num = 12;
+// console.log(typeof num);
+
+//boolean
+
+// let bool = true;
+// console.log(typeof bool);
+
+// undefined
+
+// let x ;
+// console.log(x)
+
+// null
+
+// let emptyCup = null;
+// console.log(emptyCup);
+
+//Nan
+
+// console.log(2 - "c");
+
+// operator
+
+// +,-,*,/,% // return number
+
+// =,==,===,!=,!==,<,>,<=,>= // return true or false
+
+
+// if else
+
+// falsy 
+// 1 0 
+// 2 null 
+// 3 undefined 
+// 4 NaN 
+// 5 "" 
+// 6 false
+// console.log(Boolean(0))
+
+// if(false){
+//   console.log(true);
+// }else{
+//   console.log(false);
+// }
+
+
+// let name = "john";
+// if(name == "john doe"){
+//   console.log("he is JD...")
+// }else{
+//   console.log("I don't know him...");
+// }
+
+// let name = "";
+// if(name){
+//   console.log("hello " + name);
+// }else{
+//   console.log(undefined)
+// }
+
+
+// AND gate
+// &&
+
+// let age = 20;
+
+// if(age > 20 && age < 30){
+//   console.log(true);
+// }else{
+//   console.log(false)
+// }
+
+// OR gate
+// ||
+
+// if(age > 20 || age < 20){
+//   console.log(true);
+// }else{
+//   console.log(false);
+// }
+
+
+
