6db44d28481727945fe7e86d3f56ae33739410bc
minthuhlaing <minthuhlaing786@gmail.com>
2024-09-16 23:02:28 +0630

    adding img & favorite list

--- vue.js
+++ vue.js
@@ -1,31 +1,36 @@
 const app = Vue.createApp({
-  data(){
-    return{
-      books:
-      [
+  data() {
+    return {
+      books: [
         {
-          title:"Harry Potter",
-          author:"J K Rolling",
-          gender:"Female",
-          age:33
+          title: "Harry Potter",
+          author: "J K Rolling",
+          gender: "Female",
+          age: 33,
+          img: "imgs/hacker.png",
+          isFav: false,
         },
         {
-          title:"Lord of the Rings",
-          author:"John",
-          gender:"Male",
-          age:22
+          title: "Lord of the Rings",
+          author: "John",
+          gender: "Male",
+          age: 22,
+          img: "imgs/hacker.png",
+          isFav: true,
         },
         {
-          title:"Game of Thrones",
-          author:"Alice",
-          gender:"Male",
-          age:44
-        }
+          title: "Game of Thrones",
+          author: "Alice",
+          gender: "Male",
+          age: 44,
+          img: "imgs/hacker.png",
+          isFav: false,
+        },
       ],
-      showIcon:true,
-      link:"https://google.com",
-      link1:"https://youtube.com"
-    }
-  }
-})
-app.mount("#app")
+      showIcon: true,
+      link: "https://google.com",
+      link1: "https://youtube.com",
+    };
+  },
+});
+app.mount("#app");
