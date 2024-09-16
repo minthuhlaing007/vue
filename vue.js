80fcc19def037b43cc5571c7fffbcb6fb7dbf284
minthuhlaing <minthuhlaing786@gmail.com>
2024-09-16 22:03:59 +0630

    v-for

--- vue.js
+++ vue.js
@@ -1,44 +1,29 @@
-// const app = Vue.createApp({
-//   data(){
-//     return{
-//       name:"Min Thu Hlaing",
-//       age:32,
-//       gender:"Male",
-//       showInfo: true,
-//     }
-//   }
-// })
-
-// app.mount("#app")
-
-// const app = Vue.createApp({
-//   data(){
-//     return{
-//       name:"hunny kyaw",
-//       age:44,
-//       gender:"Female",
-//       showInfo: true
-//     }
-//   }
-// })
-
-// app.mount("#app")
-
 const app = Vue.createApp({
-  methods: {
-    mouseoverHandler(event, data) {
-      console.log("mouse over is working...", event.type, data);
-      document.querySelector("#div1").style.background = "red";
-    },
-    mouseleaveHandler(event, data) {
-      console.log("mouse leave is working...", event.type, data);
-      document.querySelector("#div2").style.background = "blue";
-    },
-    mousedbHandler(event, data) {
-      console.log("mouse db is working...", event.type, data);
-      document.querySelector("#div3").style.background = "green";
-    },
-  },
-});
-
-app.mount("#app");
+  data(){
+    return{
+      books:
+      [
+        {
+          title:"Harry Potter",
+          author:"J K Rolling",
+          gender:"Female",
+          age:33
+        },
+        {
+          title:"Lord of the Rings",
+          author:"John",
+          gender:"Male",
+          age:22
+        },
+        {
+          title:"Game of Thrones",
+          author:"Alice",
+          gender:"Male",
+          age:44
+        }
+      ],
+      showIcon:true
+    }
+  }
+})
+app.mount("#app")
