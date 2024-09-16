921de30ee6e6f89fcbe5e8795cef4a28ed985db9
minthuhlaing <minthuhlaing786@gmail.com>
2024-09-17 01:13:13 +0630

    computed :

--- vue.js
+++ vue.js
@@ -1,97 +1,54 @@
-// First Method
-
-// const app = Vue.createApp({
-//   data() {
-//     return {
-//       books: [
-//         {
-//           title: "Harry Potter",
-//           author: "J K Rolling",
-//           gender: "Female",
-//           age: 33,
-//           img: "imgs/hacker.png",
-//           isFav: false,
-//         },
-//         {
-//           title: "Lord of the Rings",
-//           author: "John",
-//           gender: "Male",
-//           age: 22,
-//           img: "imgs/hacker.png",
-//           isFav: false,
-//         },
-//         {
-//           title: "Game of Thrones",
-//           author: "Alice",
-//           gender: "Male",
-//           age: 44,
-//           img: "imgs/hacker.png",
-//           isFav: false,
-//         },
-//       ],
-//       showIcon: true,
-//       link: "https://google.com",
-//       link1: "https://youtube.com",
-//     };
-//   },
-// });
-// app.mount("#app");
-
-//Second Method
-
 const app = Vue.createApp({
-  data(){
-    return{
-      books:
-      [
+  data() {
+    return {
+      books: [
         {
-          title:"You don't know Js",
-          author:"Min Thu Hlaing",
-          age:33,
-          gender:"Male",
-          img:"imgs/hacker.png",
-          isFav:false,
+          title: "game of throne",
+          author: "min thu hlaing",
+          age: 32,
+          gender: "Male",
+          img: "./imgs/hacker.png",
+          isFav: false,
         },
         {
-          title:"You don't know Html",
-          author:"Hlaing Min Than",
-          age:34,
-          gender:"Male",
-          img:"imgs/hacker.png",
-          isFav:false,
+          title: "Harry Potter",
+          author: "Huny Kyaw",
+          age: 44,
+          gender: "Female",
+          img: "./imgs/hacker.png",
+          isFav: true,
         },
         {
-          title:"You don't know Css",
-          author:"Alice",
-          age:44,
-          gender:"Female",
-          img:"imgs/hacker.png",
-          isFav:false
+          title: "Fast and Furious",
+          author: "The Rock",
+          age: 55,
+          gender: "Male",
+          img: "./imgs/hacker.png",
+          isFav: false,
         },
         {
-          title:"Harry Potter",
-          author:"JK Rolling",
-          age:55,
-          gender:"Female",
-          img:"imgs/hacker.png",
-          isFav:false
-        },
-        {
-          title:"Game of Thrones",
-          author:"Emma Stone",
-          age:99,
-          gender:"Female",
-          img:"imgs/hacker.png",
-          isFav:false
+          title: "SpiderMan",
+          author: "Mary Jane",
+          age: 11,
+          gender: "Female",
+          img: "./imgs/hacker.png",
+          isFav: true,
         },
       ],
-      bookInfo:true
-    }
+      bookInfo: true,
+    };
   },
-  methods:{
-    fav(book){
-      book.isFav = !book.isFav
-    }
-  }
-})
-app.mount("#app")
+  methods: {
+    handler(book) {
+      book.isFav = !book.isFav;
+    },
+  },
+  computed: {
+    filteredBooks() {
+      return this.books.filter((book) => {
+        return book.isFav;
+      });
+    },
+  },
+});
+app.mount("#app");
